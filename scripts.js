60301b135e47c758f3ff6517b93883de28f43a7f
Brunielle Rodrigues <143230665+BrunielleRd@users.noreply.github.com>
2023-09-25 20:34:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
-
-const tasksContainer = document.querySelector(newFunction());
+const tasksContainer = document.querySelector(".tasks-container");
 
 const validateInput = () => inputElement.value.trim().length > 0;
 
@@ -10,44 +8,41 @@ const handleAddTask = () => {
     const inputIsValid = validateInput();
 
     if (!inputIsValid) {
-        return inputElement.classList.add("error")
+        inputElement.classList.add("error");
+    } else {
+        inputElement.classList.remove("error");
+
+        const taskItemContainer = document.createElement("div");
+        taskItemContainer.classList.add("task-item");
+
+        const taskContent = document.createElement("p");
+        taskContent.innerText = inputElement.value;
+
+        const deleteItem = document.createElement("i");
+        deleteItem.classList.add("far", "fa-trash-alt");
+
+        deleteItem.addEventListener("click", () =>
+            handleDeleteClick(taskItemContainer, taskContent)
+        );
+
+        taskItemContainer.appendChild(taskContent);
+        taskItemContainer.appendChild(deleteItem);
+
+        tasksContainer.appendChild(taskItemContainer);
+
+        inputElement.value = "";
+
+        updateLocalStorage();
+    }
 };
-}
 
+const handleDeleteClick = (taskItemContainer, taskContent) => {
 
-const taskItemContainer = document.createElement("div");
-taskItemContainer.classList.add("task-item");
+};
 
+const updateLocalStorage = () => {
 
-const taskContent = document.createElement("p");
-taskContent.innerText = inputElement.value;
+};
 
-
-taskContent.addEventListener("click", () => handleClick(taskContent));
-
-
-const deleteItem = document.createElement ("i");
-deleteItem.classList.add("far");
-deleteItem.classList.add("fa-trash-alt");
-
-deleteItem.addEventListener("click", () =>
-    handleDeleteClick(taskItemContainer, taskContent)
-);
-
-taskItemContainer.appendChild(taskContent);
-taskItemContainer.appendChild(deleteItem);
-
-tasksContainer.appendChild(taskItemContainer);
-
-inputElement.value = "0";
-
-updateLocalStorage();
-
-
-function newFunction() {
-    return ".tasks-container";
-}
-
-addTaskButton.addEventListener("click",() => handleAddTask ());
-inputElement.addEventListener("change",() => handleInputChange ());
-
+addTaskButton.addEventListener("click", handleAddTask);
+inputElement.addEventListener("change", handleAddTask)
